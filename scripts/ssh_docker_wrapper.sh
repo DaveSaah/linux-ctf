132d586c3bd9d7438602178bf763313ff13412aa
David Saah <davesaah@gmail.com>
2024-10-29 17:14:45 +0000

    chore: set resource usage limit

--- scripts/ssh_docker_wrapper.sh
+++ scripts/ssh_docker_wrapper.sh
@@ -7,5 +7,8 @@ CONTAINER_NAME="$LEVEL"_"$(date | sha1sum | awk '{print $1}' | cut -c 1-7)"
 
 docker run --rm --name $CONTAINER_NAME \
 	-v "/home/davesaah/codes/linux-ctf/scripts/levels/$LEVEL:/home/$HOST/ctf" \
+	--memory="10m" \
+	--cpus="0.5" \
+	--network=challenge_network \
 	--hostname $LEVEL \
 	--user $HOST -it level
