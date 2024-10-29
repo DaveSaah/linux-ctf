5d872b711309e5377cc25d7f762f33858d67efc5
David Saah <davesaah@gmail.com>
2024-10-29 16:43:26 +0000

    fix: use absolute path

--- scripts/ssh_docker_wrapper.sh
+++ scripts/ssh_docker_wrapper.sh
@@ -6,6 +6,6 @@ HOST=linux
 CONTAINER_NAME="$LEVEL"_"$(date | sha1sum | awk '{print $1}' | cut -c 1-7)"
 
 docker run --rm --name $CONTAINER_NAME \
-	-v "./levels/$USERNAME:/home/$HOST/ctf" \
+	-v "/home/davesaah/codes/linux-ctf/scripts/levels/$LEVEL:/home/$HOST/ctf" \
 	--hostname $LEVEL \
 	--user $HOST -it level
