558cd73f4f56f0c895fe62e8cbbac337ffc10107
David Saah <davesaah@gmail.com>
2024-10-29 16:19:32 +0000

    feat: create levels

---
+++ scripts/init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+level_users=()
+
+# Loop through each level
+for ((i = 1; i <= LEVEL_COUNT; i++)); do
+	level="level$i"
+	level_dir="/home/davesaah/codes/linux-ctf/scripts/levels/$level"
+
+	# create level
+	sudo useradd --home=$level_dir -s /bin/bash $level
+
+	# # Add the user to the docker group for Docker access
+	# sudo usermod -aG docker $level
+	#
+	# # Set directory permissions
+	# sudo chmod a+rx $level_dir
+	# sudo chown root:$level $level_dir
+	#
+	# # Add readme
+	# echo "# $level" | sudo tee $level_dir/README.md >/dev/null
+	# sudo chmod a+rx $level_dir/README.md
+	# sudo chown root:$level $level_dir/README.md
+	#
+	echo "$level created."
+	level_users+=("$level")
+done
+
+# update ssh config to point to docker wrapper script
+IFS=',' # set Internal Field Separator
+levels_string="${level_users[*]}"
+unset IFS
+
+sudo sed -i '/^Match User level.*/, $d' /etc/ssh/sshd_config
+echo -e "Match User ${levels_string}
+\tForceCommand /home/davesaah/codes/linux-ctf/scripts/ssh_docker_wrapper.sh
+\tPermitTunnel no
+\tAllowTcpForwarding no
+\tX11Forwarding no" | sudo tee -a /etc/ssh/sshd_config >/dev/null
+
+sudo systemctl restart sshd
+echo "Levels created successfully."
